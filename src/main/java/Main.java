f4f4173bb4fb9f0f2052350f0e7d3ea2238888ce
kushalkanavi <kushalkanavi@gmail.com>
2017-05-26 11:01:50 -0700

    modified:   src/main/java/Main.java 	copied:     src/main/java/Main.java -> src/test/java/HashTableTest.java 	modified:   target/classes/Main.class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,26 +6,5 @@ import java.io.FileNotFoundException;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        HashTable ht = new HashTable(10);
-
-
-        ht.insert("Apple");
-        ht.insert("Ball");
-        ht.insert("Cat");
-        ht.insert("Dog");
-        ht.insert("Elephant");
-        ht.insert("Fish");
-        ht.insert("Goat");
-        ht.insert("GoatA");
-        ht.insert("GoatB");
-        ht.insert("GoatAA");
-
-        ht.printHashTable();
-
-        ht.remove("work");
-
-        ht.printHashTable();
-
-        //System.out.println(ht.get(45));
     }
 }
--- src/test/java/HashTableTest.java
+++ src/test/java/HashTableTest.java
@@ -1,7 +1,27 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by kushalkanavi on 5/26/17.
  */
 public class HashTableTest {
+
+    @Test
+    public void HashTest() throws Exception{
+        HashTable ht = new HashTable(10);
+
+        ht.insert("Apple");
+        ht.insert("Ball");
+        ht.insert("Cat");
+        ht.insert("Dog");
+        ht.insert("Elephant");
+        ht.insert("Fish");
+        ht.insert("Goat");
+        ht.insert("GoatA");
+        ht.insert("GoatB");
+        ht.insert("GoatAA");
+
+        Assert.assertEquals("Apple",ht.get("Apple"));
+        Assert.assertEquals("Elephant",ht.get("Elephant"));
+    }
 }
