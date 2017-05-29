89abc09cba4bb4a5b81936678ccadcab39e5e4ad
kushalkanavi <kushalkanavi@gmail.com>
2017-05-29 11:24:24 -0700

    modified:   src/main/java/Main.java 	modified:   src/main/java/XMLFile.java i	modified:   target/classes/Main.class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,42 +13,9 @@ import java.io.IOException;
  * Created by kushalkanavi on 5/19/17.
  */
 public class Main {
-
     public static void main(String[] args) throws FileNotFoundException, ParserConfigurationException {
-        DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dbuilder = dbfactory.newDocumentBuilder();
 
-        try {
-            Document doc = dbuilder.parse("/Users/kushalkanavi/Documents/Study Material/JAVA Programs/myprojects/src/main/resources/simple.xml");
-            doc.getDocumentElement().normalize();
-            NodeList Product = doc.getElementsByTagName("book");
-
-            for (int i=0 ;i<Product.getLength();++i){
-                Node node = Product.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE){
-                    Element element = (Element) node;
-
-                    String Author = element.getElementsByTagName("author").item(0).getTextContent().trim();
-                    String Title = element.getElementsByTagName("title").item(0).getTextContent().trim();
-                    String Genre = element.getElementsByTagName("genre").item(0).getTextContent().trim();
-                    String Price = element.getElementsByTagName("price").item(0).getTextContent().trim();
-                    String PublishDate = element.getElementsByTagName("publish_date").item(0).getTextContent().trim();
-                    String Description = element.getElementsByTagName("description").item(0).getTextContent().trim();
-                    String Descrepl = Description.trim().replaceAll("[\\n]           ","");
-
-                    System.out.println("Author: "+Author);
-                    System.out.println("Title: "+ Title);
-                    System.out.println("Genre: "+Genre);
-                    System.out.println("Price: "+Price);
-                    System.out.println("Publish-Date: "+PublishDate);
-                    System.out.println("Description: "+Descrepl);
-                    System.out.println();
-                }
-            }
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        XMLFile xf = new XMLFile();
+        xf.getClass();
     }
 }
--- src/main/java/XMLFile.java
+++ src/main/java/XMLFile.java
@@ -18,20 +18,46 @@ public class XMLFile {
             Document doc = dBuilder.parse("/Users/kushalkanavi/Documents/Study Material/JAVA Programs/myprojects/src/main/resources/project.xml");
             doc.getDocumentElement().normalize();
             NodeList items = doc.getElementsByTagName("item");
+            NodeList media = doc.getElementsByTagName("media:thumbnail");
 
-            for(int i = 0; i < items.getLength(); ++i) {
-                Node node = items.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-                    String title = element.getElementsByTagName("title").item(0).getTextContent().trim();
-                    String description = element.getElementsByTagName("description").item(0).getTextContent().trim();
-                    String category = element.getElementsByTagName("category").item(0).getTextContent().trim();
-                    String pubDate = element.getElementsByTagName("pubDate").item(0).getTextContent().trim();
-                    System.out.println(
-                            "Title : " + title + " Description : " + description + " Category : " + category +
-                                    " Publish Date" + pubDate
-                    );
+            int  mediainit=0,mediacomp =6;
+            int  iteminit =0,itemcomp =1;
+
+
+            for (int k=0;k<items.getLength();++k){
+
+                for (int i = mediainit; i < mediacomp; ++i) {
+                    Node mnode = media.item(i);
+                    if (mnode.getNodeType() == Node.ELEMENT_NODE) {
+                        Element melement = (Element) mnode;
+
+                        String URL = melement.getAttribute("url");
+                        String Width = melement.getAttribute("width");
+                        String Length = melement.getAttribute("height");
+                        String Credit = melement.getAttribute("credit");
+
+                        System.out.println("URL: "+ URL+" | Width: "+ Width+" | Length: "+ Length+" | Credit: "+ Credit);
+                    }
                 }
+                mediainit += 6; mediacomp +=6;
+
+                for (int j=iteminit;j<itemcomp;++j){
+                    Node node = items.item(j);
+                    if (node.getNodeType() == Node.ELEMENT_NODE){
+                        Element element = (Element) node;
+
+                        String title = element.getElementsByTagName("title").item(0).getTextContent().trim();
+                        String description = element.getElementsByTagName("description").item(0).getTextContent().trim();
+                        String category = element.getElementsByTagName("category").item(0).getTextContent().trim();
+                        String pubDate = element.getElementsByTagName("pubDate").item(0).getTextContent().trim();
+
+                        System.out.println(
+                                "Title : " + title + " Description : " + description + " Category : " + category +
+                                        " Publish Date" + pubDate);
+                    }
+                }
+                iteminit += 1; itemcomp += 1;
+                System.out.println();
             }
         } catch (SAXException e) {
             e.printStackTrace();
