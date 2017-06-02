dec6e4d0089ef916521cb5c3fbfd94486d84a312
kushalkanavi <kushalkanavi@gmail.com>
2017-06-01 20:38:30 -0700

    modified:   myprojects.iml 	modified:   pom.xml 	modified:   src/main/java/ExerciseThread.java 	modified:   src/main/java/Main.java 	modified:   target/classes/Main.class

--- src/main/java/ExerciseThread.java
+++ src/main/java/ExerciseThread.java
@@ -1,3 +1,5 @@
+import java.util.IllegalFormatException;
+
 /**
  * Created by kushalkanavi on 5/30/17.
  */
@@ -11,15 +13,37 @@ public class ExerciseThread extends Thread {
         this.end = end;
     }
 
-    public void run()  {
+    public void run() throws RuntimeException {
         for (int i = start; i < end; ++i){
-            System.out.println(Thread.currentThread().getName() + " - (A)Iteration Value: "+i);
+            System.out.println(Thread.currentThread().getName() + " - (A)Iteration Value: "+i+" Thread");
+            try {
+                sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        throw new IllegalArgumentException("foo");
+    }
+}
+
+class ThreadRunnable implements Runnable{
+
+    private int Start;
+    private int End;
+
+    public ThreadRunnable(int start, int end) {
+        Start = start;
+        End = end;
+    }
+
+    public void run() {
+        for (int i = Start; i < End; ++i){
+            System.out.println(Thread.currentThread().getName() + " - (A)Iteration Value: "+i+" Runnable");
             try {
                 Thread.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        throw new IllegalArgumentException("foo");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by kushalkanavi on 5/19/17.
@@ -10,13 +13,25 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         List<ExerciseThread> threads = new ArrayList<ExerciseThread>();
 
+
+        Scanner numberofthread = new Scanner(System.in);
+        System.out.println("Enter number of threads to be created");
+        int nothread = numberofthread.nextInt();
+
+        ExecutorService executor = Executors.newFixedThreadPool(nothread);
+
+        Scanner countofthread = new Scanner(System.in);
+        System.out.println("Enter number of times thread should count");
+        int cothread = countofthread.nextInt();
+
+        System.out.println();
+
         // No of threads, How much should thread count
-        for (int x = 0; x < 100; x += 10) {
-            ExerciseThread thread = new ExerciseThread(x, x + 10);
+        for (int x = 0; x < (nothread*cothread); x += cothread) {
+            ExerciseThread thread = new ExerciseThread(x, x + cothread);
             thread.start();
             threads.add(thread);
         }
-
         for(ExerciseThread thread : threads) {
             try {
                 thread.join();
@@ -24,5 +39,21 @@ public class Main {
                 e.printStackTrace();
             }
         }
+
+        System.out.println();
+
+        for (int x = 0; x < (nothread*cothread); x += cothread) {
+            ThreadRunnable thread = new ThreadRunnable(x, x + cothread);
+            Thread t = new Thread(thread);
+            t.start();
+        }
+
+        //System.out.println();
+
+        /*for (int x = 0; x < (nothread*cothread); x += cothread) {
+            ExerciseThread thread = new ExerciseThread(x, x + cothread);
+            executor.execute(new Thread(thread));
+        }
+        executor.shutdown();*/
     }
 }
